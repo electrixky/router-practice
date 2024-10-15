b3b9e889e4dd92a6f996723f36382fa32710ca13
electrixky <tanyatkachuk2401@gmail.com>
2024-10-15 17:49:37 +0300

    routing set up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from '../assets/react.svg'
+import {Link} from "react-router-dom";
 
-function Navbar(props) {
+function Navbar() {
     return (
         <div className="navbar">
             <img src={logo} alt="" width={"60px"}/>
             <ul>
-                <li>Home</li>
-                <li>Products</li>
-                <li>About</li>
-                <li>Contact</li>
+                <Link to={'/'}>
+                    <li>Home</li>
+                </Link>
+                <Link to={'/products'}>
+                    <li>Products</li>
+                </Link>
+                <Link to={'/about'}>
+                    <li>About</li>
+                </Link>
+                <Link to={'/contact'}>
+                    <li>Contact</li>
+                </Link>
             </ul>
             <button>Get Started</button>
         </div>
