fbc4daefaf55c2549c431ba3accc69b9e4e7b179
electrixky <tanyatkachuk2401@gmail.com>
2024-10-17 14:57:56 +0300

    routing useing RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
-import {Routes, Route} from "react-router-dom";
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
 import Products from "./pages/Products.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
+import RootLayout from "./layout/RootLayout.jsx";
 
 function App() {
 
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path='/' element={<RootLayout/>}>
+                <Route index element={<Home/>}/>
+                <Route path='products' element={<Products/>}/>
+                <Route path='about' element={<About/>}/>
+                <Route path='contact' element={<Contact/>}/>
+            </Route>
+        )
+    )
+
     return (
-        <div>
-            <Navbar/>
-            <div className="container">
-                <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/products' element={<Products/>}/>
-                    <Route path='/about' element={<About/>}/>
-                    <Route path='/contact' element={<Contact/>}/>
-                </Routes>
-            </div>
-        </div>
+        <RouterProvider router={router}/>
+
+        // {/*<Navbar/>*/}
+        // {/*<div className="container">*/}
+        // {/*    <Routes>*/}
+        // {/*<Route path='/' element={<Home/>}/>*/}
+        // {/*<Route path='/products' element={<Products/>}/>*/}
+        // {/*<Route path='/about' element={<About/>}/>*/}
+        // {/*<Route path='/contact' element={<Contact/>}/>*/}
+        // {/*</Routes>*/}
+        // {/*</div>*/}
     )
 }
 
