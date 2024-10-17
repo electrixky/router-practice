fc660dd7e46d5d600ecccb519d1faed8f85d8336
electrixky <tanyatkachuk2401@gmail.com>
2024-10-17 18:19:11 +0300

    dynamic routing

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import {useLoaderData, useParams} from "react-router-dom";
 
 function JobDetails() {
 
-    const {id} = useParams()
+    const jobDetails = useLoaderData()
+    // const {id} = useParams()
 
     return (
-        <div>
-            <h1>ID is: {id}</h1>
+        <div className={"jobDetails"}>
+            {/*<h1>ID is: {id}</h1>*/}
+            <p><b>Job Title: </b>{jobDetails.title}</p>
+            <p><b>Salary: </b>{jobDetails.salary}</p>
+            <p><b>Job Location: </b>{jobDetails.location}</p>
+            <p><b>Description: </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum earum modi repellendus
+                soluta tenetur? Ab debitis expedita, incidunt maiores natus placeat quaerat voluptate? Ad blanditiis
+                consequuntur ipsa iste vitae voluptatibus.</p>
+            <button>Apply Now</button>
         </div>
     );
 }
 
 export default JobDetails;
+
+export const jobDetailsLoader = async ({params}) => {
+    const {id} = params
+    const res = await fetch("http://localhost:5000/jobs/" + id)
+    return res.json()
+}
